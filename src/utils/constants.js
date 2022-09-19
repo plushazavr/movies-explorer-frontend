390a76a07f5f0423571a2442d62187a17f0d053e
Ekaterina Kazantseva <dr.kazantseva.ekaterina@gmail.com>
2022-09-19 19:31:28 +0300

    base url

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,5 @@
-//export const BASE_URL = 'https://api.diploma.kazantseva.nomoredomains.sbs';
-export const BASE_URL = 'http://localhost:3000';
+export const BASE_URL = 'https://api.diploma.kazantseva.nomoredomains.sbs';
+//export const BASE_URL = 'http://localhost:3000';
 
 export const BEATFILM_URL = 'https://api.nomoreparties.co/beatfilm-movies';
 export const BEATFILM_SOURCE_URL = 'https://api.nomoreparties.co';
