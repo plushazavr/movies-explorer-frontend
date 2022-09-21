65b1705b2f2aaf5fa63a9b7a397f6e49ec3da39a
Ekaterina Kazantseva <dr.kazantseva.ekaterina@gmail.com>
2022-09-21 22:42:16 +0300

    вынести auth в отдельный файл

---
+++ src/utils/auth.js
@@ -0,0 +1,64 @@
+const baseURL = 'http://localhost:3000';
+//
+
+function checkRes(res) {
+    if (res.ok) {
+        return res.json();
+    } else {
+        return Promise.reject(`ERROR! => ${res.status}`);
+    }
+}
+
+export const register = (name, email, password) => {
+    return fetch(`${baseURL}/signup`, {
+        method: "POST",
+        headers: {
+            "Accept": "application/json",
+            "Content-Type": "application/json",
+        },
+        credentials: 'include',
+        body: JSON.stringify({ name, email, password }),
+    })
+        .then(checkRes)
+};
+
+export const login = (email, password) => {
+    return fetch(`${baseURL}/signin`, {
+        method: "POST",
+        headers: {
+            "Accept": "application/json",
+            "Content-Type": "application/json",
+        },
+        credentials: 'include',
+        body: JSON.stringify({
+            email: email,
+            password: password,
+        }),
+    })
+        .then(checkRes)
+};
+
+export const logout = () => {
+    return fetch(`${baseURL}/signout`, {
+        method: "GET",
+        headers: {
+            "Accept": "application/json",
+            "Content-Type": "application/json",
+        },
+        credentials: 'include',
+    })
+}
+
+
+export const checkToken = () => {
+    return fetch(`${baseURL}/users/me`, {
+        method: "GET",
+        headers: {
+            "Accept": "application/json",
+            "Content-Type": "application/json",
+        },
+        credentials: 'include',
+    })
+        .then(checkRes)
+        .then((res) => res)
+};
